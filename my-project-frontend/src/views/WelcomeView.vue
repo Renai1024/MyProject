0eeea4911787d9589fcf36bdfe85cc5ce507330a
renai <2568092604@qq.com>
2024-08-21 10:32:33 +0800

    登录及退出的前端页面功能加上部分后端邮件注册，后端暂时有bug

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,38 @@
+
+<script setup>
+
+</script>
+
+<template>
+
+  <div style = "width: 100vw; height: 100vh; overflow: hidden; display: flex">
+    <div style="flex: 1; background-color: black">
+      <el-image style="width: 100%; height: 100%" fit="cover"
+                src="src/assets/image/zgl.png"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px; font-weight: bold">欢迎来到本测试平台</div>
+      <div style="margin-top: 10px">学习如何搭建网站</div>
+    </div>
+    <div class="right-card">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px;
+}
+
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: white;
+}
+</style>
